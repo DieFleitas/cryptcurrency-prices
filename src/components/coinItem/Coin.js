9ef3a3fcf52724c9b2e6cb8ad3175140e41b504b
DieFleitas <diegofleitas98@outlook.es>
2022-02-19 18:56:14 -0300

    project reseted

--- src/components/coinItem/Coin.js
+++ src/components/coinItem/Coin.js
@@ -10,29 +10,9 @@ const Coin = ({
   image,
   priceChange,
 }) => {
-  return (
-    <div className="cryptoCoin">
-      <img src={image} alt={`${name}`} className="coinLogo" />
-      <div className="coinNameWrap">
-        <h1 className="coinName">{name}</h1>
-        <p className="coinSymbol">{symbol}</p>
-      </div>
-      <p className="coinPrice">${price.toLocaleString()}</p>
-      <p className="coinMarketcap">Market Cap: ${marketcap.toLocaleString()}</p>
-      <p className="coinVolume">Volume (24H): ${volume.toLocaleString()}</p>
-      {priceChange < 0 ? (
-        <div className="priceContainerDOWN">
-          <i className="fas fa-angle-down fa-2x"></i>
-          <p className="priceChange">{priceChange.toFixed(2)}%</p>
-        </div>
-      ) : (
-        <div className="priceContainerUP">
-          <i className="fas fa-angle-up fa-2x"></i>
-          <p className="priceChange">{priceChange.toFixed(2)}%</p>
-        </div>
-      )}
-    </div>
-  );
+  return ( 
+    <div>Coin</div>
+   );
 };
 
 export default Coin;
