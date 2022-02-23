c8f3813482a23f1ab41aa3b0c6b33a85b0274914
DieFleitas <diegofleitas98@outlook.es>
2022-02-23 18:58:22 -0300

    getting the data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import "./App.css";
 import Coin from "./components/coinItem/Coin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then((res) => {
+      setCoins(res.data);
+    })
+    .catch((error) => console.log(error))
+  })
   return (
     <div>
       <div className="header">
