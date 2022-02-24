8fb80519a0fc29112dfda81fdd2e646c0a9e47e9
DieFleitas <diegofleitas98@outlook.es>
2022-02-24 17:11:15 -0300

    project done

--- src/components/coinItem/Coin.js
+++ src/components/coinItem/Coin.js
@@ -17,13 +17,27 @@ const Coin = ({
         <h1 className="coinName">{name}</h1>
         <p className="coinSymbol">{symbol}</p>
       </div>
-      <p className="coinPrice">{price}</p>
+      <p className="coinPrice">$ {price.toLocaleString()}</p>
       <p className="coinMarketcap">
-        Market Cap: {marketcap}
+        Market Cap: {marketcap.toLocaleString()}
       </p>
       <p className="coinVolume">
-        Volume (24H): $ {volume}
+        Volume (24H): $ {volume.toLocaleString()}
       </p>
+
+      {priceChange < 0 ? 
+        (
+          <div className="priceContainerDOWN">
+            <i className="fas fa-angle-down fa-2x"/>
+            <p className="priceChange">{priceChange.toFixed(2)}%</p>
+          </div>  
+        ) : (
+          <div className="priceContainerUP">
+            <i className="fas fa-angle-up fa-2x"/>
+            <p className="priceChange">{priceChange.toFixed(2)}%</p>
+          </div>  
+        )
+      }
     </div>
    );
 };
